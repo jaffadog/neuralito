fc87a97518d77b49e5cb096d9d000d11281dda44
maxipaolucci <maxipaolucci@d2f8c090-cc5a-11dd-a880-a1081c7ac358>
2008-12-22 00:55:25 +0000

    implementacion de la diferencia de horas maxima permitida, faltaria chequear que pasa si me cambia de dia

--- trunk/neuralito/ArfGen/src/filter/ww3Filter/WW3CouplingFilter.java
+++ trunk/neuralito/ArfGen/src/filter/ww3Filter/WW3CouplingFilter.java
@@ -11,10 +11,12 @@ import buoy.BuoyData;
 import filter.Filter;
 
 public class WW3CouplingFilter extends Filter {
-	Collection buoyData;
+	private Collection buoyData;
+	private int maxHoursDifference = 12;
 
-	public WW3CouplingFilter(Collection buoyData) {
+	public WW3CouplingFilter(Collection buoyData, int maxHourDifference) {
 		this.buoyData = buoyData;
+		this.maxHoursDifference = maxHoursDifference;
 	}
 
 	@Override
@@ -22,26 +24,47 @@ public class WW3CouplingFilter extends Filter {
 		Collection<WaveWatchData> wwDatas = new Vector<WaveWatchData>();
 		for (Iterator iterator = buoyData.iterator(); iterator.hasNext();) {
 			BuoyData buoy = (BuoyData) iterator.next();
-			WaveWatchData wwData = getMoreRecentWaveWAtchOfBuoy(buoy,
-					WW3DataSet);
-			wwDatas.add(wwData);
+			WaveWatchData wwData = getMoreRecentWaveWAtchOfBuoy(buoy, WW3DataSet);
+			if (wwData != null)
+				wwDatas.add(wwData);
 		}
 		return (Vector<?>) wwDatas;
 	}
 
-	private WaveWatchData getMoreRecentWaveWAtchOfBuoy(BuoyData buoy,
-			Vector<?> WW3DataSet) {
+	private WaveWatchData getMoreRecentWaveWAtchOfBuoy(BuoyData buoy, Vector<?> WW3DataSet) {
 		Long buoyTime = buoy.getDate().getTimeInMillis();
 		WaveWatchData prev = null;
+		WaveWatchData prox = null;
+		
 		for (Iterator iterator = WW3DataSet.iterator(); iterator.hasNext();) {
 			WaveWatchData wwData = (WaveWatchData) iterator.next();
-			if (wwData.getDate().getTimeInMillis() < buoyTime)
+			if (wwData.getDate().getTimeInMillis() <= buoyTime)
 				prev = wwData;
-			else
+			else{
+				prox = wwData;
 				break;
-
+			}
 		}
-		return prev;
+		prev = this.checkHoursDifference(prev, buoy);
+		prox = this.checkHoursDifference(prox, buoy);
+		
+		if (prev == null && prox == null)
+			return null;
+		else if (prev == null )
+			return prox;
+		else if (prox == null)
+			return prev;
+		else if (Math.abs(prev.getDate().getTimeInMillis() - buoyTime) <= Math.abs(prox.getDate().getTimeInMillis() - buoyTime))
+			return prev;
+		else
+			return prox;
 	}
-
+	
+	private WaveWatchData checkHoursDifference(WaveWatchData wwData, BuoyData buoy){
+		long maxHourDiffMillisec = this.maxHoursDifference * 3600000; 
+		if (Math.abs(wwData.getDate().getTimeInMillis() - buoy.getDate().getTimeInMillis()) > maxHourDiffMillisec)
+			return null;
+		return wwData;
+	}
+	
 }
