c52e3d1619b26a2953d4b845e21062b0bf6106b7
wagner.esteban <wagner.esteban@d2f8c090-cc5a-11dd-a880-a1081c7ac358>
2009-08-31 12:05:15 +0000

    New Project structure created. Multimodule maven project. Modules are: Common: common interfaces between gwt and server Server: implementation of interfaces. gui: gwt application which uses the server.

---
+++ trunk/surf-forecaster/common/src/main/java/edu/unicen/surfforecaster/client/SampleRemoteService.java
@@ -0,0 +1,7 @@
+package edu.unicen.surfforecaster.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+public interface SampleRemoteService extends RemoteService {
+	public String doComplimentMe();
+}
