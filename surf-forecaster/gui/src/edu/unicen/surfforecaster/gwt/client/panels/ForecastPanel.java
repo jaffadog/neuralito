15448bc8e197685067f1490f68627141ad419bfb
maxipaolucci <maxipaolucci@d2f8c090-cc5a-11dd-a880-a1081c7ac358>
2009-09-08 20:49:56 +0000

    forecast panel first approach

--- surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/panels/ForecastPanel.java
+++ surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/panels/ForecastPanel.java
@@ -1,11 +1,22 @@
 package edu.unicen.surfforecaster.gwt.client.panels;
 
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
 
-public class ForecastPanel extends VerticalPanel {
+public class ForecastPanel extends FlexTable {
 
 	public ForecastPanel() {
-		// TODO Auto-generated constructor stub
+		CurrentForecastPanel current = new CurrentForecastPanel("Ahora");
+		CurrentForecastPanel nextHours = new CurrentForecastPanel("+ 3 horas");
+		
+		this.setWidget(0, 0, current);
+		this.setWidget(0, 1, nextHours);
+		this.setWidget(1, 0, new Image("images/windguru.PNG"));
+		this.getFlexCellFormatter().setColSpan(1, 0, 2);
+		this.getCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		this.getCellFormatter().setHorizontalAlignment(0, 1, HasHorizontalAlignment.ALIGN_CENTER);
+		this.getCellFormatter().setHorizontalAlignment(1, 0, HasHorizontalAlignment.ALIGN_CENTER);
 	}
 
 }
