2dbdadd44ac7fbeffffce072209bd09a39bf361b
wagner.esteban <wagner.esteban@gmail.com>
2009-09-23 12:12:28 +0000

    removed test service, and added user service to SurfForecasterCommonServicesImplementation.java. Now login is performed against the database using the UserService.java

--- surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/ForecastCommonServices.java
+++ surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/ForecastCommonServices.java
@@ -7,9 +7,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
-import edu.unicen.surfforecaster.common.services.dto.HelloDTO;
-
-
 @RemoteServiceRelativePath("ForecastCommonServices")
 public interface ForecastCommonServices extends RemoteService {
 	/**
@@ -17,31 +14,28 @@ public interface ForecastCommonServices extends RemoteService {
 	 */
 	public static class Util {
 		private static ForecastCommonServicesAsync instance;
-		public static ForecastCommonServicesAsync getInstance(){
+
+		public static ForecastCommonServicesAsync getInstance() {
 			if (instance == null) {
 				instance = GWT.create(ForecastCommonServices.class);
 			}
 			return instance;
-		}	
+		}
 	}
-	
-	String testService();
-	
+
 	User login(String userName, String password);
-	
+
 	SessionData getSessionData();
-	
+
 	void closeSession();
-	
-	public HelloDTO test2();
-	
+
 	Map<String, Vector> getAreas();
-	
+
 	Map<String, Vector> getCountries(String area);
-	
+
 	Map<String, Vector> getZones(String country);
-	
+
 	Map<String, Vector> getSpots(String spot);
-	
+
 	Area getArea();
 }
