5c76391fdd67af334428db7d7497b6a247318d6e
maxipaolucci <maxipaolucci@d2f8c090-cc5a-11dd-a880-a1081c7ac358>
2009-11-25 12:36:42 +0000

    gwt visualizations integration + column chart in spots comparator

--- surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/panels/ComparationViewerPanel.java
+++ surf-forecaster/gui/src/edu/unicen/surfforecaster/gwt/client/panels/ComparationViewerPanel.java
@@ -1,20 +1,31 @@
 package edu.unicen.surfforecaster.gwt.client.panels;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.visualization.client.DataTable;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.visualizations.ColumnChart;
+import com.google.gwt.visualization.client.visualizations.ColumnChart.Options;
 
+import edu.unicen.surfforecaster.common.exceptions.NeuralitoException;
 import edu.unicen.surfforecaster.common.services.dto.ForecastDTO;
+import edu.unicen.surfforecaster.common.services.dto.Unit;
+import edu.unicen.surfforecaster.common.services.dto.WW3Parameter;
 import edu.unicen.surfforecaster.gwt.client.utils.GWTUtils;
+import edu.unicen.surfforecaster.gwt.client.utils.UnitConverter;
 import edu.unicen.surfforecaster.gwt.client.widgets.HTMLButtonGrayGrad;
 
 public class ComparationViewerPanel extends FlexTable implements ISurfForecasterBasePanel, ClickHandler {
@@ -39,6 +50,9 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 	private Label spotName3 = null;
 	private Label spotName4 = null;
 	private Label spotName5 = null;
+	
+	private List<String> forecastersNames = null;
+	
 	
 	private static final String COLOR_LABEL_WIDTH = "15px";
 	private static final String COLOR_LABEL_HEIGHT = "15px";
@@ -173,7 +187,8 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 		//TODO hacer que el valor harcodedo 5 de la sig if sea una variable statica bien definida (ya se definio en el comparationCreatorPanel, tal vez habria que 
 		//meterla en otro lado)
 		if (spotsLatestForecasts.size() >= 2 && spotsLatestForecasts.size() <= 5 && spotsNames.size() >= 2 && spotsNames.size() <= 5) {
-			this.fillSpotPoperties(spotsLatestForecasts, spotsIds, spotsNames);
+			this.fillSpotProperties(spotsLatestForecasts, spotsIds, spotsNames);
+			this.drawColumnChart(spotsLatestForecasts, spotsIds, spotsNames);
 		} else {
 			//TODO el mesagebox siguiente
 			Window.alert("La cantuidad de spots a comparar tiene que ser entre 2 y 5");
@@ -181,14 +196,91 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 		}
 		
 	}
+	
+	private void drawColumnChart(final Map<Integer, Map<String, List<ForecastDTO>>> spotsLatestForecasts, final List<Integer> spotsIds, final List<String> spotsNames) {
+	    VisualizationUtils.loadVisualizationApi("1.1",
+	            new Runnable() {
+	              public void run() {
+	            	  DataTable data = createTable(spotsLatestForecasts, spotsIds, spotsNames);
+	            	  
+		          	  Options options = Options.create();
+		          	  options.setHeight(300);
+		          	  options.setTitle("Alturas de ola futuras");
+		          	  options.setWidth(950);
+		          	  options.set3D(true);
+		          	  options.setShowCategories(true);
+		          	  options.setEnableTooltip(true);
+		          	  options.setMin(0);
+		          	  options.setMax(20);
+	            	  
+		          	  ColumnChart viz = new ColumnChart(data, options);
+	          	      setWidget(5, 0, viz);
+	          	      getFlexCellFormatter().setColSpan(5, 0, 4);
+	              }
+        }, ColumnChart.PACKAGE);
+	}
+	
+	private DataTable createTable(Map<Integer, Map<String, List<ForecastDTO>>> spotsLatestForecasts, List<Integer> spotsIds, List<String> spotsNames) {
+		DataTable data = DataTable.create();
+		
+		data.addColumn(ColumnType.STRING, "Date");
+  	    
+		Iterator<String> i = spotsNames.iterator();
+  	    while (i.hasNext()) {
+  	    	String spotName = i.next();
+  	    	data.addColumn(ColumnType.NUMBER, spotName);
+  	    }
+  	    
+  	    data.addRows(4);
+  	    
+	    for (int spotIndex = 0; spotIndex < spotsIds.size(); spotIndex++) {
+	    	Integer spotId = spotsIds.get(spotIndex);
+	    	String forecasterName = this.forecastersNames.get(spotIndex);
+	    	List<ForecastDTO> forecasts = spotsLatestForecasts.get(spotId).get(forecasterName);
+	    	data.setValue(spotIndex, 0, "+n horas");
+	    	for (int forecastIndex = 0; forecastIndex < 4; forecastIndex++) {
+				ForecastDTO forecastDTO = forecasts.get(forecastIndex);
+				//TODO generar las unidades en que se ve el sitio como alguna setting de usuario o usando cookies o algo y emprolijar la manera de levantarlo
+				Unit heightUnitTarget = Unit.Meters;
+				Unit speedUnitTarget = Unit.KilometersPerHour;
+				Unit directionUnitTarget = Unit.Degrees;
+				Unit periodUnitTarget = Unit.Seconds;
+				
+				//wave height
+				String waveHeight = forecastDTO.getMap().get(WW3Parameter.COMBINED_SWELL_WIND_WAVE_HEIGHT.toString()).getValue();
+				//wind speed
+				String windSpeed = forecastDTO.getMap().get(WW3Parameter.WIND_SPEED.toString()).getValue();
+				//Wave direccion
+				String waveDirection = forecastDTO.getMap().get(WW3Parameter.PRIMARY_WAVE_DIRECTION.toString()).getValue();
+				//Wave period
+				String wavePeriod = forecastDTO.getMap().get(WW3Parameter.PRIMARY_WAVE_PERIOD.toString()).getValue();
+				try {
+					windSpeed = NumberFormat.getFormat("###.#").format(UnitConverter.convertValue(windSpeed, Unit.KilometersPerHour, speedUnitTarget));
+					waveHeight = NumberFormat.getFormat("###.#").format(UnitConverter.convertValue(waveHeight, Unit.Meters, heightUnitTarget));
+					waveDirection = NumberFormat.getFormat("###.#").format(UnitConverter.convertValue(waveDirection, Unit.Degrees, directionUnitTarget));
+					wavePeriod = NumberFormat.getFormat("###").format(UnitConverter.convertValue(wavePeriod, Unit.Seconds, periodUnitTarget));
+				} catch (NeuralitoException e) {
+					// TODO ver como manejar esta exvepcion de conversion de unidades
+					e.printStackTrace();
+				}
+				
+	    		data.setValue(forecastIndex, spotIndex + 1, new Double(waveHeight));
+			}
+	    }
+  	    
+		return data;
+	}
 
-	private void fillSpotPoperties(Map<Integer, Map<String, List<ForecastDTO>>> spotsLatestForecasts, List<Integer> spotsIds, List<String> spotsNames) {
+	private void fillSpotProperties(Map<Integer, Map<String, List<ForecastDTO>>> spotsLatestForecasts, List<Integer> spotsIds, List<String> spotsNames) {
+		this.forecastersNames = new ArrayList<String>();
+		
 		this.spotName1.setText(spotsNames.get(0));
 		Iterator<String> i = spotsLatestForecasts.get(spotsIds.get(0)).keySet().iterator();
 		while (i.hasNext()) {
 			String forecasterName = i.next();
 			this.spotBox1.addItem(forecasterName, forecasterName);
 		}
+		this.forecastersNames.add(this.spotBox1.getValue(0));
 		
 		this.spotName2.setText(spotsNames.get(1));
 		i = spotsLatestForecasts.get(spotsIds.get(1)).keySet().iterator();
@@ -196,6 +288,7 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 			String forecasterName = i.next();
 			this.spotBox2.addItem(forecasterName, forecasterName);
 		}
+		this.forecastersNames.add(this.spotBox2.getValue(0));
 		
 		if (spotsIds.size() >= 3) {
 			this.spotName3.setText(spotsNames.get(2));
@@ -207,6 +300,7 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 			this.spotName3.setVisible(true);
 			this.spotBox3.setVisible(true);
 			this.spotColor3.setVisible(true);
+			this.forecastersNames.add(this.spotBox3.getValue(0));
 			
 			if (spotsIds.size() >= 4) {
 				this.spotName4.setText(spotsNames.get(3));
@@ -218,6 +312,7 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 				this.spotName4.setVisible(true);
 				this.spotBox4.setVisible(true);
 				this.spotColor4.setVisible(true);
+				this.forecastersNames.add(this.spotBox4.getValue(0));
 				
 				if (spotsIds.size() == 5) {
 					this.spotName5.setText(spotsNames.get(4));
@@ -230,6 +325,7 @@ public class ComparationViewerPanel extends FlexTable implements ISurfForecaster
 					this.spotName5.setVisible(true);
 					this.spotBox5.setVisible(true);
 					this.spotColor5.setVisible(true);
+					this.forecastersNames.add(this.spotBox5.getValue(0));
 				}
 			}
 		}
